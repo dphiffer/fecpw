f67730f558c9bfb9f06c102e15e7b7b9cb37feb7
Dan Phiffer <dan@phiffer.org>
2011-02-15 08:38:32 -0500

    Added theme with css

---
+++ 04/theme_with_css/header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+    <title><?php bloginfo('title'); ?></title>
+    <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" />
+  </head>
+  <body>
+    <div id="container">
+      <div id="header"></div>
